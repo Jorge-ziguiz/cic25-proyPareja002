46ce23626e8a73dbd3203a2cb72253268b572223
n <n@n.n>
2025-07-21 13:03:23 +0200

    subida al repo

---
+++ src/main/java/cic25/proyPareja002/grupo8/app/AppApplication.java
@@ -0,0 +1,13 @@
+package cic25.proyPareja002.grupo8.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cic25/proyPareja002/grupo8/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package cic25.proyPareja002.grupo8.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
