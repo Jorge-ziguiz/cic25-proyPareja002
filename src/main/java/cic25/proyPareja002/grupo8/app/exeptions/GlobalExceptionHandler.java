9c66fe7402757ab95ff9f1a7522caf1ca2830caf
n <n@n.n>
2025-07-22 13:38:05 +0200

    resolcuin de conflictos

--- src/main/java/cic25/proyPareja002/grupo8/app/exeptions/GlobalExceptionHandler.java
+++ src/main/java/cic25/proyPareja002/grupo8/app/exeptions/GlobalExceptionHandler.java
@@ -5,15 +5,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import cic25.proyPareja002.grupo8.app.service.TiendaNoExistia;
+import cic25.proyPareja002.grupo8.app.service.TiendaNulaException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({
             SecureNoAllowNewID.class,
+            TiendaNulaException.class,
+            TiendaNoExistia.class,
 
     })
 
     public ResponseEntity<Object> handleSecureNoAllowNewID(SecureNoAllowNewID e) {
         return new ResponseEntity<Object>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
+
+    public ResponseEntity<String> handleTiendaNoExistiaException(TiendaNoExistia e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Error: " + e.getMessage());
+    }
+
+    public ResponseEntity<String> handleTiendaNulaException(TiendaNulaException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Error: " + e.getMessage());
+    }
+
 }
